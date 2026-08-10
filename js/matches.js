29226ed9aa09998a63098ff4c86704cda213c2cc
Noschi22 <n.schink@svholtsee.de>
2026-08-10 10:30:14 +0200

    fit

--- js/matches.js
+++ js/matches.js
@@ -7,14 +7,9 @@ const matches = {
             highlight: true
         },
         away: {
-<<<<<<< HEAD
             name: "Eckernförder SV",
 
             logo: "assets/esv.png",
-=======
-            name: "SVE Comet Kiel II",
-            logo: "assets/comet.jpg",
->>>>>>> dc6a9ca871a4a1f90ded693dcd81a1343a8d6593
             highlight: false
         }
     },
