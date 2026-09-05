d35ca18e714e9148ddb531cabbf7aaa73735e0cb
noschi22 <fussballobmann@svholtsee.de>
2026-09-05 14:41:52 +0200

    Merge branch 'main' of https://github.com/Noschi22/SV-Holtsee-Wittensee-Touch

--- js/matches.js
+++ js/matches.js
@@ -1,29 +1,29 @@
 const matches = {
     team1: {
-        date: "2026-08-23T15:00:00",
+        date: "2026-09-05T16:00:00",
         home: {
             name: "SV Holtsee/Wittensee Liga",
             logo: "assets/logo.png",
             highlight: true
         },
         away: {
-            name: "Rendsburger TSV",
+            name: "Osterrönfelder TSV II",
 
-            logo: "assets/rtsv.png",
+            logo: "assets/otsv.png",
             highlight: false
         }
     },
 
     team2: {
-        date: "2026-08-23T13:00:00",
+        date: "2026-09-06T13:00:00",
         home: {
             name: "SV Holtsee/Wittensee Zwoote",
             logo: "assets/logo.png",
             highlight: true
         },
         away: {
-            name: "SG Osdorf/Schinkel",
-            logo: "assets/osdorf.jpg",
+            name: "SV Schwansen II",
+            logo: "assets/schwansen.png",
             highlight: false
         }
     }
